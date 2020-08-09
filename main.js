143c747eb9d8b3bd36bb0e3f242b528eb1fc8811
Magdalena Falkowska-Legieć <magda.f41@gmail.com>
2020-08-09 20:07:13 +0200

    zadanie_35

---
+++ main.js
@@ -0,0 +1,10 @@
+let n;
+function range(n) {
+
+    if (n > 100 && n < 200) {
+        return console.log("Liczba znajduje się w przedziale");
+    } else {
+        return console.log("Liczba nie znajduje się w przedziale");
+    }
+}
+range(150);
